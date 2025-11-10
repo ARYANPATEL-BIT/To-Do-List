732711db6000fd5efb3ba70d8dc0cb1c6361108b
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2025-11-10 18:41:27 +0530

    Added normal Adding and removing feature

--- scritpt.js
+++ scritpt.js
@@ -1,8 +1,47 @@
-const value = document.getElementById("value").value;
-const task = document.getElementById("task");
-
 function append(){
     
-    value = "";
+    const newTask = document.createElement("div");
+    newTask.id = "newTasks";
+    newTask.className = "newTasks"
 
+    const done = document.createElement("button");
+    done.id = "done";
+    done.className = "done"
+    done.textContent = "✔️"
+
+    let value = document.getElementById("val").value;
+    newTask.textContent = value;
+
+    const task = document.getElementById("task");
+    task.appendChild(newTask);
+    newTask.appendChild(done)
+
+    done.onclick = () => newTask.remove();
+
+    document.getElementById("val").value = "";
+
+}
+
+function Priority(){
+   
+    const impTask = document.createElement("div");
+    impTask.id = "impTask";
+    impTask.className = "impTask"
+
+    const done = document.createElement("button");
+    done.id = "done";
+    done.className = "done"
+    done.textContent = "✔️"
+
+    let value = document.getElementById("val").value;
+    impTask.textContent = value;
+
+    const task = document.getElementById("task");
+    task.appendChild(impTask);
+    impTask.appendChild(done)
+
+    done.onclick = () => impTask.remove();
+
+    document.getElementById("val").value = "";
+    
 }
